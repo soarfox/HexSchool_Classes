df4350ff89a5337872ac9fee74af85b743737d9a
Howard <soarfox@gmail.com>
2023-08-22 18:05:29 +0800

    add four categories pages and search result page

---
+++ RWD_TheF2E_TaiwanTour/js/attractions.js
@@ -0,0 +1,7 @@
+// 以下語法可直接放在網址列上敲, 能夠取得資料
+
+// https://ptx.transportdata.tw/MOTC/v2/Tourism/Hotel?$filter=Class ne '民宿' & top=300
+
+// https://ptx.transportdata.tw/MOTC/v2/Tourism/Hotel?$filter=Class ne '民宿' and Class ne '一般旅館' & top=30
+
+// https://ptx.transportdata.tw/MOTC/v2/Tourism/Hotel?$filter=Class ne '民宿' and Class ne '一般旅館' and Class ne '一般觀光旅館' and Class ne '國際觀光旅館' & top=30
