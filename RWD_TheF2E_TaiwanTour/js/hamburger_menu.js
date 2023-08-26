e40209691fc642fb834c2fa49d8447bb5a3fc4b4
Howard <soarfox@gmail.com>
2023-08-26 15:22:17 +0800

    add aria-label to pages

--- RWD_TheF2E_TaiwanTour/js/hamburger_menu.js
+++ RWD_TheF2E_TaiwanTour/js/hamburger_menu.js
@@ -2,7 +2,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
   const navElement = document.querySelector('.navbar');
-  console.log(navElement);
 
   // 一旦mobileMenuIcon被點擊, 先阻擋預設點擊會發生的行為後, 並為navbar元素加上/刪去(因為toggle代表切換的意思)一個class名稱'show-mobile-menu', 以利CSS內對應的語法能夠產生效果
   mobileMenuIcon.addEventListener('click', function(e) {
